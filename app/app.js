c3d7527653a28953d166296451fadb5274bf4c38
Mapleiny <i@maple.im>
2016-08-11 18:09:47 +0800

    session change

--- app/app.js
+++ app/app.js
@@ -9,7 +9,7 @@ app.set('views', 'views');
 app.set('view engine', 'ejs');
 app.use(partials());
 app.use(express.static('public'));
-app.use(session({ secret: 'maple' }));
+app.use(session({ secret: 'maple',resave: true,saveUninitialized: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router_1.router);
